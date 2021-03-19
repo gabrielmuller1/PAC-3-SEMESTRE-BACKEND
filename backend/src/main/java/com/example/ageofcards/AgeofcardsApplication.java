40f33e9a6caa88401f78f0bddce0a28ad5e92da2
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-03-19 07:45:15 -0300

    Project Age of Cards have been created

---
+++ backend/src/main/java/com/example/ageofcards/AgeofcardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ageofcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeofcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeofcardsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ageofcards/AgeofcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ageofcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeofcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
